6eb0718f4d9c7ce06baba95a6b121210d63ae20c
Valpik97 <114172485+Valpik97@users.noreply.github.com>
2022-11-22 20:12:57 +0300

    Add files via upload

---
+++ ege_24/7_ege.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	string str;
+	char last;
+	char new1;
+	int max_lentgth = 1, cur_length = 1;
+	ifstream in("C:\\Users\\111040\\c++\\ABD.txt");
+	in >> last;
+	while(in >> new1){
+    	if((last == 'A' and new1 == 'D') or (last == 'A' and new1 == 'B')){
+        	cur_length ++;
+        }
+    	else{
+        	cur_length = 1;
+		}
+		last = new1;
+		if(cur_length > max_lentgth){
+        		max_lentgth = cur_length;
+		}
+	}
+	cout << max_lentgth;
+	return 0;
+}
