62b633daec9fc05c156fdcaa3a529a4303b7faf0
Valpik97 <114172485+Valpik97@users.noreply.github.com>
2022-11-22 19:13:19 +0300

    Update 2_ege.cpp

--- ege_24/2_ege.cpp
+++ ege_24/2_ege.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 int main()
 {
-	ifstream("D:\\c++\\ABD.txt");
+	ifstream("C:\\Users\\111040\\c++\\ABD.txt");
 	string str;
 	char last;
 	char new1;
-	int max_lentgth = 0, cur_length;
-	auto in = ifstream("D:\\c++\\ABD.txt");
+	int max_lentgth = 0, cur_length = 0;
+	ifstream in("C:\\Users\\111040\\c++\\ABD.txt");
 	in >> last;
 	while(in >> new1){
-    	if((last == 'B' and new1 == 'D') or (last == 'B' and new1 == 'A')){
-        	cur_length = 1;
-        	last = new1;
+    	if((last != 'B' and new1 != 'D') or (last != 'B' and new1 != 'A')){
+        	cur_length ++;
         }
     	else{
-        	cur_length++;
+        	cur_length = 0;
 		}
+		last = new1;
 		if(cur_length > max_lentgth){
         		max_lentgth = cur_length;
 		}
