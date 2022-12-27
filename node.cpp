d5343ebe0c7bcdb125c86826f7d1e49580c5f7c5
Valpik97 <114172485+Valpik97@users.noreply.github.com>
2022-12-27 19:38:38 +0300

    Add files via upload

---
+++ node.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+Node{
+	int value;
+	Node* next
+}
+
+class stack{
+public:
+	Node* first;
+	void push(int a){
+		Node* tmp = new Node;
+		tmp -> value = a;
+		tmp -> next = first;
+		first = tmp;
+	}
+	int pop(int b){
+		Node* tmp = new Node;
+		
+		
+	}
+	
+};
+
+
+int main(){
+	
+}
